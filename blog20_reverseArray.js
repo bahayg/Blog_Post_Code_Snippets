9103f59e628685b0eb8234eb7854e2242ebb0154
bahayg <bahayg@gmail.com>
2020-08-14 23:02:48 -0700

    Edit Post# 20

--- blog20_reverseArray.js
+++ blog20_reverseArray.js
@@ -11,21 +11,25 @@ reverseArray1 = arr => {
 
 reverseArray1(["a", "b", "c", "d"]); // [ 'd', 'c', 'b', 'a' ]
 
-
 reverseArray2 = arr => {
+    // Create an empty array
     newArr = []
+    // Iterate over the given array from the beginning
     arr.forEach(element => {
       newArr.unshift(element)})
+    // Return the reversed array
     return newArr
 }
 
 reverseArray2([1, 2, 3, 4, 5]); // [ 5, 4, 3, 2, 1 ]
 
 reverseArray3 = arr => {
-    // Loop through the halfway point of array 
+    // Loop over the half the length of the given array 
     for (let i = 0; i < Math.floor(arr.length/2); i ++) {
+      // Swap the elements with array destructuring
       [arr[i], arr[arr.length-1-i]] = [arr[arr.length-1-i], arr[i]]
     }
+    // Return the modified array
     return arr
 }
 
