0da075557a506547c05e8cc5bb6da12dd730ca2f
bahayg <bahayg@gmail.com>
2020-10-03 17:24:41 -0700

    Post# 27

---
+++ blog27_binarySearch.js
@@ -0,0 +1,11 @@
+const binarySearch = (nums, target) => {
+    let min = 0
+    let max = nums.length - 1
+    while(min <= max) {
+        let mid = Math.floor((min + max) / 2)
+        if (target === nums[mid]) return mid
+        if (target > nums[mid]) min = mid + 1
+        else max = mid - 1
+    }
+    return -1
+}
